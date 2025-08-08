cf25b0192dbadf0d064c70ae7c11f932cde7da34
Anton Gadimba <antonhck@gmail.com>
2025-08-08 23:46:55 +0300

    Added seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,6 +15,11 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             RoleSeeder::class,
+            LanguagesSeeder::class,
+            DisciplinesSeeder::class,
+            LevelsSeeder::class,
+            AchievementsSeeder::class,
+            AdminUserSeeder::class,
         ]);
 
         // User::factory(10)->create();
